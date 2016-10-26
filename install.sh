c6f56aaec992bfc4132d0c2ee890f5fa9f991eb2
Caitlyn Yu <caitlynmyu@gmail.com>
2016-10-25 22:23:19 -0700

    install command to automatically copy over configurations

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -eu
+
+cd ~
+
+cp .bash_profile      $HOME/.bash_profile
+cp .basrc             $HOME/.bashrc
+cp .gitconfig         $HOME/.gitconfig
+cp .inputrc           $HOME/.inputrc
+cp .irbrc             $HOME/.irbrc
+cp .pryrc             $HOME/.pryrc
+cp .rspec             $HOME/.rspec
