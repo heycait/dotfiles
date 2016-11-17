86731b50494a59e311b4484cbe1709019ea64f40
Caitlyn Yu <caitlynmyu@gmail.com>
2016-11-16 22:31:58 -0800

    fix install script

--- install.sh
+++ install.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 set -eu
 
-cd ~
-
 cp .bash_profile      $HOME/.bash_profile
-cp .basrc             $HOME/.bashrc
+cp .bashrc            $HOME/.bashrc
 cp .gitconfig         $HOME/.gitconfig
 cp .inputrc           $HOME/.inputrc
 cp .irbrc             $HOME/.irbrc
 cp .pryrc             $HOME/.pryrc
 cp .rspec             $HOME/.rspec
+cp .vimrc             $HOME/.vimrc
+
+echo "Dotfiles successfulled copied"
